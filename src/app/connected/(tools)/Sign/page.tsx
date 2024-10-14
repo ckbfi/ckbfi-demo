2670904a41caf9a5a6ed1d8e9eaf4ebc1d3e7718
riderOmO <kncnight@gmail.com>
2024-10-14 15:09:58 +0800

    feat:init

---
+++ src/app/connected/(tools)/Sign/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { ccc } from "@ckb-ccc/connector-react";
+import React, { useState } from "react";
+import { Button } from "../../../components/Button";
+import { TextInput } from "../../../components/Input";
+import { useApp } from "../../../context";
+import { ButtonsPanel } from "../../../components/ButtonsPanel";
+
+export default function Sign() {
+  const { signer, createSender } = useApp();
+  const { log, error } = createSender("Sign");
+
+  const [messageToSign, setMessageToSign] = useState<string>("");
+  const [signature, setSignature] = useState<string>("");
+  return (
+    <div className="flex w-full flex-col items-stretch">
+      <TextInput
+        label="Message"
+        placeholder="Message to sign and verify"
+        state={[messageToSign, setMessageToSign]}
+      />
+      <ButtonsPanel>
+        <Button
+          onClick={async () => {
+            if (!signer) {
+              return;
+            }
+            const sig = JSON.stringify(await signer.signMessage(messageToSign));
+            setSignature(sig);
+            log("Signature:", sig);
+          }}
+        >
+          Sign
+        </Button>
+        <Button
+          className="ml-2"
+          onClick={async () => {
+            if (
+              !(await ccc.Signer.verifyMessage(
+                messageToSign,
+                JSON.parse(signature),
+              ))
+            ) {
+              error("Invalid");
+              return;
+            }
+            log("Valid");
+          }}
+        >
+          Verify
+        </Button>
+      </ButtonsPanel>
+    </div>
+  );
+}
---
+++ src/app/connected/page.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import React, { ReactNode, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { icons } from "lucide-react";
+import { BigButton } from "../components/BigButton";
+
+/* eslint-disable react/jsx-key */
+const TABS: [ReactNode, string, keyof typeof icons, string][] = [
+  ["Sign", "/connected/Sign", "Signature", "text-orange-500"],
+  ["Transfer", "/connected/Transfer", "ArrowLeftRight", "text-lime-500"],
+  ["Transfer xUDT", "/connected/TransferXUdt", "BadgeCent", "text-emerald-500"],
+];
+/* eslint-enable react/jsx-key */
+
+export default function Page() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    TABS.forEach(([_, path]) => router.prefetch(path));
+  }, [router]);
+
+  return (
+    <div className="flex flex-wrap justify-center gap-8 px-4 lg:px-16">
+      {TABS.map(([name, link, iconName, classes]) => (
+        <BigButton
+          key={link}
+          size="sm"
+          iconName={iconName}
+          onClick={() => router.push(link)}
+          className={classes}
+        >
+          {name}
+        </BigButton>
+      ))}
+    </div>
+  );
+}
